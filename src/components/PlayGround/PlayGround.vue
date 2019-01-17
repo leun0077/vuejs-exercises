5b169b6c737f7adcdbbdf932712e5f55663a9683
Gabriel Leung <leun0077@algonquinlive.com>
2019-01-17 11:57:47 -0500

    initializing PlayGround with Separation of Concern

---
+++ src/components/PlayGround/PlayGround.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="hello">
+    <button>CLick</button>
+  </div>
+</template>
+<script>
+src = "./PlayGround.js";
+</script>
+<style src = "./PlayGround.js" scoped>
+</style>
