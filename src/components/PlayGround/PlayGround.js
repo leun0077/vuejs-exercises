3c628fc79944a99b1ef24228159ec344a2d01942
Gabriel Leung <leun0077@algonquinlive.com>
2019-01-24 12:34:38 -0500

    syntax exploration

--- src/components/PlayGround/PlayGround.js
+++ src/components/PlayGround/PlayGround.js
@@ -1,3 +1,46 @@
 export default {
-  name: 'PlayGround'
+  name: 'PlayGround',
+  data() {
+    return {
+      backgroundClassName: 'red-background',
+      counter: 0,
+      message: 'static message',
+      anchorTag: '<a href="http://google.ca">{{ message }}</a>',
+      nameInput: 'No Named'
+    }
+  },
+  watch: {
+    nameInput(newValue, oldValue) {
+      /* eslint-disable no-console */
+      console.log('old value', oldValue)
+      console.log('new value', newValue)
+    }
+  },
+  computed: {
+    reversedMessage() {
+      return this.message
+        .split('')
+        .reverse()
+        .join('')
+    }
+  },
+  methods: {
+    handleClick() {
+      this.counter++
+      // String interpolation
+      this.message = `you are clicking this ${this.counter}Times!`
+      // tenary operator
+      // new way with ?
+      this.counter % 2 === 0
+        ? (this.backgroundClassName = 'green-background')
+        : (this.backgroundClassName = 'red-background')
+      // Normal way of coding
+      // if (this.counter % 2 === 0) {
+      //   this.backgroundClassName = 'green-background'
+      // } else {
+      //   this.backgroundClassName = 'red-background'
+      // }
+      this.message = this.nameInput
+    }
+  }
 }
--- src/components/PlayGround/PlayGround.vue
+++ src/components/PlayGround/PlayGround.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="hello">
-    <button>Click</button>
+    <!-- Watch -->
+    <input type="text" name="firstName" v-model="nameInput">
+    <!-- Computed -->
+    <div>{{message}} => Reversed: {{ reversedMessage}}</div>
+    <!-- V-Model -->
+    <label for="firstName">First Name</label>
+    <input type="text" name="firstName" v-model="nameInput">
+    <!-- V-html -->
+    <div v-html="anchorTag"></div>
+    <!-- v-once -->
+    <div v-once>The first message will never change: {{ message }}</div>
+    <div>The first message will never change: {{ message }}</div>
+    <!-- V-on -->
+    <button v-on:click="handleClick()">Click This</button>
+    <!-- V-Bind Shorthand -->
+    <div v-bind:class="backgroundClassName">Some Class with background</div>
   </div>
 </template>
 <script src = "./PlayGround.js"></script>
